571a7de95fa38530a5b9596fe74d127a071c0186
rashidgafurov <nsd7c4@gmail.com>
2022-03-07 16:39:48 -0500

    Homework 3 Assignment 1

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -3,6 +3,12 @@ package com.RashidGafurov_INC_LOL;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //Assignment 1
+        int clientOS = 1;
+        if (clientOS == 1){
+            System.out.println("Please install Android App, link here");
+        } else {
+            System.out.println("Please install iOS App, link here");
+        }
     }
 }
