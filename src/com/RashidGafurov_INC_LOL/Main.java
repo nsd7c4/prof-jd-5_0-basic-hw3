05176f4cd3391c3bf394d13a381edeb90fbe93d3
rashidgafurov <nsd7c4@gmail.com>
2022-03-07 16:49:35 -0500

    Homework 3 Assignment 2

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -3,12 +3,28 @@ package com.RashidGafurov_INC_LOL;
 public class Main {
 
     public static void main(String[] args) {
-    //Assignment 1
+        //Assignment 1
         int clientOS = 1;
-        if (clientOS == 1){
+        if (clientOS == 1) {
             System.out.println("Please install Android App, link here");
         } else {
             System.out.println("Please install iOS App, link here");
+        }
+
+        //Assignment 2
+        int clientDeviceYear = 2014;
+        if (clientOS == 1 ) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Please install Android App, link here");
+            } else {
+                System.out.println("Please install Lite Android App, link here");
+            }
+        } else {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Please install iOS App, link here");
+            } else {
+                System.out.println("Please install Lite iOS App, link here");
+            }
         }
     }
 }
