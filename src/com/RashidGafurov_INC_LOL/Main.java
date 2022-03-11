e411297bff6d7bcf05c0e165791105d7d0816e96
rashidgafurov <nsd7c4@gmail.com>
2022-03-11 10:49:27 -0500

    Homework 3 Assignment 6 and 7

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int clientOS = 1;
         if (clientOS == 1) {
             System.out.println("Please install Android App, link here");
-        } else if (clientOS == 0){
+        } else if (clientOS == 0) {
             System.out.println("Please install iOS App, link here");
         }
 
@@ -19,7 +19,7 @@ public class Main {
             } else {
                 System.out.println("Please install Lite Android App, link here");
             }
-        } else if (clientOS == 0){
+        } else if (clientOS == 0) {
             if (clientDeviceYear >= 2015) {
                 System.out.println("Please install iOS App, link here");
             } else {
@@ -74,5 +74,51 @@ public class Main {
             default:
                 System.out.println("This month doesn't exist!");
         }
+
+        //Assignment 6
+        int clientAge = 19;
+        int clientSalary = 58_000;
+        int creditLimit = 0;
+
+        if (clientAge >= 23) {
+            creditLimit = clientSalary * 3;
+        } else {
+            creditLimit = clientSalary * 2;
+        }
+
+        if (clientSalary >= 80000) {
+            creditLimit *= 1.5;
+        } else if (clientSalary >= 50000) {
+            creditLimit *= 1.2;
+        }
+
+        System.out.println("We are ready to issue you a credit card with limit of " + creditLimit + " rubles.");
+
+        //Assignment 7
+        clientAge = 25;
+        clientSalary = 60000;
+        int wantedSum = 330000;
+        float apr = 10;
+        int loanLength = 12;
+
+        if (clientAge < 23) {
+            apr += 1f;
+        } else if (clientAge < 30) {
+            apr += 0.5f;
+        }
+        if (clientSalary > 80000) {
+            apr -= 0.7f;
+        }
+
+        float creditWithIntrestMonthly = wantedSum * (1 + apr / (float) 100) / loanLength;
+
+        System.out.print("Maximum payment for salary of " + clientSalary + " rubles is " + clientSalary / 2 + " rubles. Loan payment is " + creditWithIntrestMonthly + " rubles.");
+
+        if (creditWithIntrestMonthly > clientSalary / 2) {
+            System.out.println(" Denied.");
+        } else {
+            System.out.println(" Approved.");
+        }
+
     }
 }
